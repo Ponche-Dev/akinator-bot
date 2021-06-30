ce6e4e40acc68c5856308bc1b23b55c59e39dac4
Ponche <67195412+Ponche-Dev@users.noreply.github.com>
2021-06-30 19:09:36 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+module.exports = client => {
+
+    client.user.setPresence({activity: {name: "Developed By Ponche#6600", type: "PLAYING"}, status: "idle"});
+
+};
+
+//---------------------------------------------------
+
+//type: PLAYING, WATCHING
+
+//status: online, idle, dnd 
+
