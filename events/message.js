3d3f99b0d0f248e95cee4dfa370a4d5a95cfa5b4
Ponche <67195412+Ponche-Dev@users.noreply.github.com>
2021-06-30 19:09:04 +0300

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,30 @@
+const ayarlar = require("../ayarlar.json");
+
+module.exports = message => {
+
+    let client = message.client;
+
+    if (!message.content.startsWith(ayarlar.prefix)) return;
+
+    let command = message.content.toLowerCase().split(" ")[0].slice(ayarlar.prefix.length)
+
+    let params = message.content.split(" ").slice(1)
+
+    let cmd;
+
+    if (client.commands.has(command)) {
+
+        cmd = client.commands.get(command);
+
+    } else if (client.aliases.has(command)) {
+
+        cmd = client.commands.get(client.aliases.get(command));
+
+    };
+
+    
+
+};
+
+//----------BURAYI KURCALAMA------------------
+
