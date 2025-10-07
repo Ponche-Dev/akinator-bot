cffde566cf3a13f88f65ff02b1af0d488074d009
Ponche <67195412+Ponche-Dev@users.noreply.github.com>
2025-10-08 00:34:39 +0300

    ç

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,11 @@
 module.exports = client => {
 
-    client.user.setPresence({activity: {name: "Developed By Ponche#6600", type: "PLAYING"}, status: "idle"});
+    client.user.setPresence({activity: {name: "Developed By AnılK", type: "PLAYING"}, status: "dnd"});
 
 };
 
 //---------------------------------------------------
-
-//type: PLAYING, WATCHING
-
-//status: online, idle, dnd 
-
+//type: PLAYING, WATCHING !!!! Sağdaki değerler Oynuyor/İzliyor
+//status: online, idle, dnd  !!!! Sırasıyla: Çevrimiçi, Boşta, Rahatsız Etmeyin
+//Şuanda rahatsız etme modundadır bota hiçbir katkısı yok o yüzden 3 değeri istediğiniz gibi atayabilirisiniz
+// Name kısmını istediğiniz gibi ayarlayabilirsiniz, Type/Status değerlerini yukarıdaki değerlerden başka girmeyin.
